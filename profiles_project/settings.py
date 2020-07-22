05b1063cc491345fedeb7a438d7eece7f64f8116
justpiyoosh <justpiyoosh@gmail.com>
2020-07-22 23:25:49 +0530

    Updated Setting File

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
+
+
+STATIC_ROOt = 'static/'
